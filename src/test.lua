2f6c1742fb4c2943bfb52fbd22c8c978faed3d32
skitsanos <info@skitsanos.com>
2023-01-16 11:37:18 +0200

    chore: added exmaple on how to use pagination

--- src/test.lua
+++ src/test.lua
@@ -1,4 +1,5 @@
 local arangodb = require("arangodb")
+local json = require('cjson')
 
 local db = arangodb.new({
     endpoint = "http://127.0.0.1:8529",
@@ -22,5 +23,39 @@ if success then
     end
 else
     -- print the error message
-    print(results)
+    print('ERR: ' .. results)
+end
+
+
+-- page size
+local page_size = 25
+
+-- starting page number
+local page_num = 1
+
+-- loop until all pages are retrieved
+while true do
+    -- execute a query
+    local successPaginated, resultsPaginated = pcall(function()
+        return db:query("FOR doc IN users LIMIT " .. (page_num - 1) * page_size .. ", " .. page_size .. " RETURN doc")
+    end)
+
+    if not successPaginated then
+        print('ERR: ' .. resultsPaginated)
+        break
+    else
+        -- print the results
+        print('Getting results, page #' .. page_num .. ', size: ' .. #resultsPaginated)
+        for _, v in ipairs(resultsPaginated) do
+            print(json.encode(v))
+        end
+
+        -- if the number of returned documents is less than page size, we have reached the end
+        if #resultsPaginated < page_size then
+            break
+        end
+
+        -- increment page number
+        page_num = page_num + 1
+    end
 end
